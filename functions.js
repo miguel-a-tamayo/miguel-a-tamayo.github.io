5e111b614243617c0f60ef4d85e9f82e4e771439
miguel-a-tamayo <miiguell098@gmail.com>
2021-08-28 18:04:52 -0700

    Responsive Navigation
    Added a menu button and changes the look of the navigation bar for phones

---
+++ functions.js
@@ -0,0 +1,22 @@
+var menuList = document.getElementById("navigation-list")
+var menuButton = document.getElementById("menu-button")
+var listNames = document.getElementsByClassName("link")
+
+function toggleMenu() {
+    if (getComputedStyle(menuList).display == "none") {
+        menuList.style.display = "flex"
+        console.log(getComputedStyle(menuList).display)
+        return
+    }
+
+    if (getComputedStyle(menuList).display == "flex") {
+        menuList.style.display = "none"
+        console.log(getComputedStyle(menuList).display)
+        return
+    }
+
+}
+
+function test() {
+    console.log("Hello World!")
+}

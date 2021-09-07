79591b066ebee9a9590e5780a66cabefb3c972a4
miguel-a-tamayo <miiguell098@gmail.com>
2021-09-06 21:56:36 -0700

    Phone Interactive Menu
    Updated the menu to fade in and out for phones!

--- newfunctions.js
+++ newfunctions.js
@@ -1,15 +1,31 @@
 $(document).ready(function() {
+    var delay_time = 0;
 
     $(".menu-button").click(function() {
         if ($("#navigation-list").css("display") == "none") {
             $("#navigation-list").css("display", "flex");
+
+            $("li").hide().each(function (i) {
+                $(this).delay(delay_time).fadeIn();
+                delay_time += 50;
+            })
+
+            delay_time = 0; // reset time before exiting
+
             return
         }
 
         if ($("#navigation-list").css("display") == "flex") {
-            $("#navigation-list").css("display", "none")
+            $($("li").get().reverse()).each(function (i) {
+                $(this).delay(delay_time).fadeOut();
+                delay_time += 50;
+            })
 
-            $("#navigation-list").css("display", "none");
+            delay_time = 0;
+
+            setTimeout(function() {
+                $("#navigation-list").css("display", "none");
+            }, 500)
             return
         }
     });
